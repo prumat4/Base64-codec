3fcd9c037f2a65436ed0f64b1f3e1289a2de2eb8
maksymdidukh <maxym.didukh@logivations.com>
2023-11-12 18:07:21 +0100

    change pathes to files in main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "base64/base64.hpp"
 
 int main() {
-    std::string test = "../files/test.txt";
-    std::string encodedFile = "../files/test-encoded.txt";
-    std::string decodedFile = "../files/test-decoded.txt";
+    std::string test = "../demonstration-files/test.txt";
+    std::string encodedFile = "../demonstration-files/test-encoded.txt";
+    std::string decodedFile = "../demonstration-files/test-decoded.txt";
 
     Coder coder(test);
     coder.encodeFile(encodedFile);
